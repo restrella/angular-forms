9a10b73bb41fedbc4edc84df96760118e34c81b1
learndev77 <restrella@77global.biz>
2024-03-15 10:59:56 +0800

    template driven form

--- src/app/modules/templateform/pages/template-list/template-list.component.ts
+++ src/app/modules/templateform/pages/template-list/template-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Hero } from 'src/app/shared/models/hero';
 
 @Component({
@@ -8,5 +9,13 @@ import { Hero } from 'src/app/shared/models/hero';
 })
 export class TemplateListComponent {
   powers = ['Fly', 'Zap', 'Stick', 'Lazer Eyes', 'Frozen Breath'];
-  model = new Hero(20, 'Iron Man', this.powers[1], 'Tony Stark');
+  model = new Hero(20, '', this.powers[1], 'Tony Stark');
+
+  // onSubmit = () => {};
+
+  onSubmit(f: NgForm) {
+    console.log(this.model);
+    console.log(f.value);
+    console.log(f.valid);
+  }
 }
